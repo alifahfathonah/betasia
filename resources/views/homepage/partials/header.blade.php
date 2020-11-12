9561e71908ce7b7bcdaa87c2b2f22f2f5a9bb406
Fajar Siagian <fajar7xx@gmail.com>
2020-11-12 08:06:29 +0700

    homepage design

---
+++ resources/views/homepage/partials/header.blade.php
@@ -0,0 +1,32 @@
+<head>
+    <meta charset="utf-8">
+    <title>Educenter</title>
+
+    <!-- mobile responsive meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- ** Plugins Needed for the Project ** -->
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="homepage/plugins/bootstrap/bootstrap.min.css">
+    <!-- slick slider -->
+    <link rel="stylesheet" href="homepage/plugins/slick/slick.css">
+    <!-- themefy-icon -->
+    <link rel="stylesheet" href="homepage/plugins/themify-icons/themify-icons.css">
+    <!-- animation css -->
+    <link rel="stylesheet" href="homepage/plugins/animate/animate.css">
+    <!-- aos -->
+    <link rel="stylesheet" href="homepage/plugins/aos/aos.css">
+    <!-- venobox popup -->
+    <link rel="stylesheet" href="homepage/plugins/venobox/venobox.css">
+
+    <!-- Main Stylesheet -->
+    <link href="homepage/css/style.css" rel="stylesheet">
+
+    <!--Favicon-->
+    <link rel="shortcut icon" href="homepage/images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="homepage/images/favicon.png" type="image/x-icon">
+
+    @yield('css')
+
+</head>
